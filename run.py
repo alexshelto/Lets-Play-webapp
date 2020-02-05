7ad3c216517f912670ecb25eb7fc0a97170c1901
alexshelto <alexandershelton@icloud.com>
2020-02-05 12:45:21 -0500

    dependencies

--- run.py
+++ run.py
@@ -2,12 +2,14 @@
 # file is used to run the webapp
 # file will call create_app from src/__init__.py uppon run
 from src import create_app
-
+from src import db
 
 #Creating the flask application
 app = create_app()
 app.app_context().push()
 
+with app.app_context():
+    db.create_all()
 
 #running the app
 if __name__ == '__main__':
