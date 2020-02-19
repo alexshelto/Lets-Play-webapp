6fefa07e1e03192adf255629aa7fe69c229ae989
alexshelto <alexandershelton@icloud.com>
2020-02-18 20:51:45 -0500

    post db

--- src/users/forms.py
+++ src/users/forms.py
@@ -2,7 +2,6 @@
 # file creates all the input form for user registration and account details
 # allong with methods of validating and reseting user email/pictures/etc
 #
-#
 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
--- src/users/routes.py
+++ src/users/routes.py
@@ -2,7 +2,6 @@
 #
 #
 #
-#
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 from src import db, bcrypt
