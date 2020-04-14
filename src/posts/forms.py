7f88e9cb89a837376126d9935d4c8783500d1538
alexshelto <alexandershelton@icloud.com>
2020-04-14 16:58:48 -0500

    Reorganization of game ids

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -9,17 +9,10 @@ from wtforms.validators import DataRequired
 
 
 
-
-Game_Choices = [('bball', 'Basketball'), ('cod', 'Call of Duty'), ('dd', 'Dungeons & Dragons'), ('vol', 'Volleyball'), ('fb', 'Football'), ('ha', 'Halo'), ('ft', 'Fortnite'), ('al', 'Apex Legends'), ('sb', 'Smash Brothers'), ('bb', 'Baseball'), ('bg', 'Board Games'), ('mg', 'Misc. Games')]
-
+Game_Choices = [('al', 'Apex Legends'),('bb', 'Baseball'),('bball', 'Basketball'),('bg', 'Board Games'),('cod', 'Call of Duty'), ('dd', 'Dungeons & Dragons'),('ft', 'Fortnite'), ('fb', 'Football'),('ha', 'Halo'),('mg', 'Misc. Games'), ('sb', 'Smash Brothers'),('vol', 'Volleyball')] 
 
 class PostForm(FlaskForm):
     game = SelectField('Game',[DataRequired()], choices=Game_Choices)
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
-
-# class PostForm(FlaskForm):
-#         title = StringField('Title', validators=[DataRequired()])
-#         content = TextAreaField('Content', validators=[DataRequired()])
-#         submit = SubmitField('Post')
